96105b988eaae7a0cc586f09a28dce87738263bf
or2372c <78048082+or2372c@users.noreply.github.com>
2025-05-10 22:44:38 +0200

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -4,14 +4,14 @@ set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
 
-#mkdir -p /config/prism
+mkdir -p /config/prism
 # mkdir -p /config/multimc/MultiMC
 # mkdir -p /config/multimc/MultiMC/bin
-#export HOME=/config/prism
-#cp /prism/PrismLauncher /config/prism/PrismLauncher
-#cp -r /prism/bin /config/prism/bin
-#cd $HOME
+export HOME=/config/prism
+cp /prism/PrismLauncher /config/prism/PrismLauncher
+cp -r /prism/bin /config/prism/bin
+cd $HOME
 
 # exec cat
-#exec PrismLauncher
+exec PrismLauncher
 # exec minecraft-launcher
