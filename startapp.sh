52ab610edc95525197ef3024668a4b830e5f99fc
or2372c <78048082+or2372c@users.noreply.github.com>
2025-05-11 17:41:31 +0200

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -8,8 +8,8 @@ mkdir -p /config/prism
 # mkdir -p /config/multimc/MultiMC
 # mkdir -p /config/multimc/MultiMC/bin
 export HOME=/config/prism
-cp /prism/PrismLauncher.AppImage /config/prism/PrismLauncher.AppImage
-cp -r /prism/bin /config/prism/bin
+#cp /prism/PrismLauncher.AppImage /config/prism/PrismLauncher.AppImage
+#cp -r /prism/bin /config/prism/bin
 cd $HOME
 
 # exec cat
